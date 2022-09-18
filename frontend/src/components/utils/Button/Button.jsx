275a8819c5909e94f3621ace38f93bf78cc31fc8
Dec <dec@Decagons-MacBook-Air.local>
2022-09-18 16:29:07 +0100

    routes and utils setup

---
+++ frontend/src/components/utils/Button/Button.jsx
@@ -0,0 +1,15 @@
+/* Code generated with AutoHTML for Figma */
+import './Button.css';
+
+export const Button = ({
+    ...props
+}) => {
+
+    return (
+        <div className="button">
+            <div className="button-text">
+                {props.text}
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/components/utils/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import LogoPNG from '../../../images/Logo.png'
+function Logo() {
+        return (
+                <div><img src={LogoPNG} alt="logo" /></div>
+        )
+}
+
+export default Logo
