faa3f566d53baef00c89ea3039ef559e933ee143
Temitope Adejolu <tadejolu@gmail.com>
2022-09-20 18:10:50 +0100

    landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import { Login } from './components/Login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {NavBar} from './components/NavBar/NavBar';
+import LandingPage from './pages/LandingPage';
+
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar/>
       <Routes>
+      <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
+       {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
     </BrowserRouter>
   );
