7e7c1868d047cda8f948d6592dbd8b8323da1100
New User <laptop@News-MacBook-Pro.local>
2022-09-22 17:24:10 +0100

    fix:before pull from develop

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -23,6 +23,7 @@ export const Signup = ({
         password: "",
         confirmpassword: ""
     })
+    // handlechange logic remove this comment later
     const handleChange = (e) => {
         setUser({ ...user, [e.target.name]: e.target.value })
         console.log(user);
