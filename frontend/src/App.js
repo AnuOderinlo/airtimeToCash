5e577f022796d2707af5c795ecd04f09f776722a
Dec <dec@Decagons-MacBook-Air.local>
2022-09-28 11:21:14 +0100

    fix:changes to responsiveness of tab components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,25 +13,29 @@ import Dashboard from './components/Dashboard/Dashboard';
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/ReactToastify.css";
 
+
+import MessageAlertModal from './components/MessageAlertModal'
+
 function App() {
   return (
-  <>
-  <ToastContainer />
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/update" element={<Update />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/verification' element={<EmailVerification />} />
-        <Route path='/users/registered' element={<UserVerified content="register" />} />
-        <Route path='/users/verify/:token' element={<UserVerified content="verify" />} />
-        <Route path='/users/change-password/:id' element={<ResetPassword />} />
-        
-      </Routes>
-    </BrowserRouter>
+    <>
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/modal" element={<MessageAlertModal showModal={true} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/update" element={<Update />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/verification' element={<EmailVerification />} />
+          <Route path='/users/registered' element={<UserVerified content="register" />} />
+          <Route path='/users/verify/:token' element={<UserVerified content="verify" />} />
+          <Route path='/users/change-password/:id' element={<ResetPassword />} />
+
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
