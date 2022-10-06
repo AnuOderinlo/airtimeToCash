1ee567b8976dba1fa594377a9a58286142f20365
UjSilvia <obianuju1994@gmail.com>
2022-10-06 10:41:51 +0100

    feat: V1

--- frontend/src/components/TransactionHistory/TransactionHistory.jsx
+++ frontend/src/components/TransactionHistory/TransactionHistory.jsx
@@ -4,54 +4,19 @@ import {Transaction, TransactionWrapper } from './TransactionHistoryStyle';
 function TransactionHistory() {
   return (
   <TransactionWrapper>
-    <Transaction> 
-      <div>
-        <p><b>Today,</b> 10:15AM</p> 
-        <p>Withdraw fund</p> 
-        <p>25/5/2022</p></div>          
-        
-        <div className='status'>
-          <label className='label'>Received</label>
-          <p>N5,000</p>
-        </div>
-    </Transaction>
-    
-    <Transaction> 
-      <div>
-        <p><b>Today,</b> 10:15AM</p> 
-        <p>Withdraw fund</p> 
-        <p>25/5/2022</p></div>          
-        
-        <div className='status'>
-          <label className='label'>Received</label>
-          <p>N5,000</p>
-        </div>
-    </Transaction>
-
-
-    <Transaction> 
-      <div>
-        <p><b>Today,</b> 10:15AM</p> 
-        <p>Withdraw fund</p> 
-        <p>25/5/2022</p></div>          
-        
-        <div className='status'>
-          <label className='label'>Received</label>
-          <p>N5,000</p>
-        </div>
-    </Transaction>
     
 
     <Transaction> 
       <div>
-        <p><b>Today,</b> 10:15AM</p> 
-        <p>Withdraw fund</p> 
-        <p>25/5/2022</p></div>          
+        <p><b>Status</b> </p> 
+        <p>Airtime amount</p> 
+        <p>25/5/2022, 10:15AM</p>
+      </div>          
         
-        <div className='status'>
-          <label className='label'>Received</label>
-          <p>N5,000</p>
-        </div>
+      <div className='status'>
+        <label className='label'>Received</label>
+        <p>N5,000</p>
+      </div>
     </Transaction>
 
 
